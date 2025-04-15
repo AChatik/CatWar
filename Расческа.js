f46dd69b7af0448bae17405ae1a73eebb9439dcf
PIPka <56842922+AChatik@users.noreply.github.com>
2025-04-15 22:46:48 +0300

    Add files via upload

---
+++ Расческа.js
@@ -0,0 +1,70 @@
+// ==UserScript==
+// @name         Расческа
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  Делает взаимодействие с сайто мприятнее
+// @author       PIPos
+// @match        https://catwar.net/cw3/
+// @icon         https://catwar.net/cw3/things/3611.png
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
+(function() { 'use strict'; })(); //это че ваще
+
+var settings = {
+  SmoothEverything: true,
+  SmoothTime: 0.2
+};
+
+const inject_CSS =`
+
+a {
+  display: inline-block; 
+  border: 2px solid transparent;
+  border-radius: 5px; 
+  transition: ${settings['SmoothTime']}s ease; 
+  text-decoration: none; 
+  color: inherit;
+}
+
+a:hover {
+  background-color: #ffffff22;
+}
+
+div.skill:hover, div.parameter:hover, .move_name:hover {
+  scale: 1.05;
+}
+
+.move_name:hover {
+  opacity: 130%;
+  -webkit-box-shadow: 0px 0px 10px 4px rgba(255, 255, 255, 0.15);
+  -moz-box-shadow: 0px 0px 10px 4px rgba(255, 255, 255, 0.15);
+  box-shadow: 0px 0px 10px 4px rgba(255, 255, 255, 0.15);
+}
+
+${settings['SmoothEverything'] ? '* {transition: '+settings["SmoothTime"]+'s ease;}' : ''}
+
+a:hover {
+  border-radius: 8px;
+  background-color: #FFFFFF33;
+}
+
+.move_name {
+  cursor: pointer;
+}
+
+button {
+  padding: 5px,0px,5px,0px;
+  border-radius: 7px;
+  cursor: pointer;
+}
+`;
+
+function inject() {
+    var head = document.querySelector("head");
+    var styleNode = document.createElement("style");
+    styleNode.innerHTML = inject_CSS;
+    head.appendChild(styleNode)
+}
+inject();
